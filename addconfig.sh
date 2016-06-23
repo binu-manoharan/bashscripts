7b88464d7d2431128b53f437ff39311175260810
Binu <binu.manoharan@i-nexus.com>
2016-06-23 09:26:13 +0100

    Fix echo messages

--- addconfig.sh
+++ addconfig.sh
@@ -5,12 +5,12 @@ function addConfig {
     echo "cp ./configtemplate $HOME/$cfgfile"
     cp ./configtemplate $HOME/$cfgfile
     if [[ -w $HOME/.profile ]]; then
-	echo "Added line to .profile: . ./$cfgfile"
+	echo "Added line to .profile: . $HOME/$cfgfile"
 	echo ". $HOME/$cfgfile" >> $HOME/.profile
     fi
     
     if [[ -w $HOME/.bashrc ]]; then
-	echo "Added line to .bashrc: . ./$cfgfile"
+	echo "Added line to .bashrc: . $HOME/$cfgfile"
 	echo ". $HOME/$cfgfile" >> $HOME/.bashrc
     fi
 }
